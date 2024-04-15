453626886af73c3acc15353c556f69d53ecc8a5c
Ayodeji Abimbola <dejibimbola@gmail.com>
2024-04-15 17:36:28 +0100

    Add app data

---
+++ app/data/metadata/twitter.ts
@@ -0,0 +1,11 @@
+const twitterMetadata = {
+    twitter: {
+        site: '@dejibimbola',
+        creator: '@dejibimbola',
+        title: 'Blog - Dejibimbola',
+        description: 'Development journey for a full stack developer.',
+        image: 'https://blog.dejibimbola.com/img/twitter-card.jpg',
+    },
+}
+
+export default twitterMetadata;
