e77feb9bb7dcfe4ceba79888bcd27441e324e1f7
Ayodeji Abimbola <dejibimbola@gmail.com>
2024-04-15 17:36:03 +0100

    Add pages and page layouts

---
+++ app/page.tsx
@@ -0,0 +1,89 @@
+import tinytime from 'tinytime'
+import Link from 'next/link'
+import { getAllPostPreviews } from '../utils/getAllPosts'
+import openGraphMetadata from './data/metadata/opengraph'
+import { PostPreview, PostPreviews } from '@/app/models/Post'
+import { Metadata } from 'next'
+
+const postDateTemplate = tinytime('{MMMM} {DD}, {YYYY}')
+
+export const metadata: Metadata = {
+  openGraph: {
+    ...openGraphMetadata,
+  },
+  title: 'Journey',
+}
+
+const Home = async () => {
+  const posts = await getPosts()
+
+  return (
+    <div className="container mx-auto px-16 py-12">
+      <div className="divide-y divide-gray-200">
+        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Journey
+          </h1>
+          <p className="text-lg leading-7 text-gray-500">
+            My journey as a developer, chronicled in a blog.
+          </p>
+        </div>
+        <ul className="divide-y divide-gray-200">
+          {posts.map(({ slug, meta, preview }) => {
+            return (
+              <li key={slug} className="py-12">
+                <article className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
+                  <dl>
+                    <dt className="sr-only">Published on</dt>
+                    <dd className="text-base font-medium leading-6 text-gray-500">
+                      <time dateTime={meta.date}>
+                        {postDateTemplate.render(new Date(meta.date))}
+                      </time>
+                    </dd>
+                  </dl>
+                  <div className="space-y-5 xl:col-span-3">
+                    <div className="space-y-6">
+                      <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                        <Link href={slug} className="text-gray-900">
+                          {meta.title}
+                        </Link>
+                      </h2>
+                      <div className="prose text-gray-500 max-w-none">
+                        <div dangerouslySetInnerHTML={{ __html: preview }} />
+                      </div>
+                    </div>
+                    <div className="text-base font-medium leading-6">
+                      <Link
+                        href={slug}
+                        className="text-yellow-400 hover:text-yellow-500"
+                        aria-label={`Read "${meta.title}"`}
+                      >
+                        Read more &rarr;
+                      </Link>
+                    </div>
+                  </div>
+                </article>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+async function getPosts(): Promise<PostPreviews> {
+  const ReactDOMServer = (await import('react-dom/server')).default
+
+  const posts = (await getAllPostPreviews()).map(({ slug, module }) => {
+    return {
+      slug,
+      meta: module.meta,
+      preview: ReactDOMServer.renderToStaticMarkup(<module.default />),
+    } as PostPreview
+  })
+
+  return posts as PostPreviews
+}
+
+export default Home
---
+++ app/posts/layout.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import React, { ReactNode, useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
+import { getPostMetadata } from '@/utils/getAllPosts'
+import { PostMetadata } from '@/app/models/Post'
+import PageTitle from '@/app/components/PageTitle'
+import PostDate from '@/app/components/PostDate'
+import Link from 'next/link'
+import ArticleNavigation from '../components/ArticleNavigation'
+import AuthorDetails from '../components/AuthorDetails'
+
+const PostLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const emptyPostMetadata: PostMetadata = {
+    title: '',
+    description: '',
+    date: '',
+    image: '',
+    link: '',
+    authors: [],
+  }
+  const [postMetadata, setPostMetadata] = useState(emptyPostMetadata)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const postMetadata = await getPostMetadata(pathname)
+
+      setPostMetadata(postMetadata)
+    }
+
+    fetchData()
+  }, [pathname])
+  return (
+    <div className="container bg-white m-16 p-16 mx-auto">
+      <div className="space-y-1 text-center">
+        <dl className="space-y-10">
+          <PostDate date={postMetadata.date} />
+        </dl>
+      </div>
+      <PageTitle>{postMetadata.title}</PageTitle>
+      <div className="border-b-2 border-gray-200 mb-8" />
+      <div className="flex flex-col-reverse md:flex-row">
+        <div className="flex-none md:w-64 md:pr-16 mt-4 md:mt-0">
+          <AuthorDetails authors={postMetadata.authors} />
+          <ArticleNavigation previous={postMetadata.previous} next={postMetadata.next} />
+        </div>
+        <article className="flex-auto prose max-w-none">{children}</article>
+      </div>
+    </div>
+  )
+}
+
+export default PostLayout
